ce9fba3003b4cd019e8b7c21838e4ffd9d9963c5
farhancdr <pyfrhn@gmail.com>
2021-06-13 20:56:09 +0600

    _

--- index.js
+++
@@ -1,7 +0,0 @@
-import Express from 'express'
-const app = Express();
-const port = 3000;  
-app.get("/",(req, res)=>{
-    res.send("Hello world")
-})
-app.listen(process.env.PORT || port,()=>console.log("Listening on port "+port))
